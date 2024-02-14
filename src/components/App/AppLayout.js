1e33afed382ee1fac4b576cc032abde13d336dfe
Wilson <wj.buena@gmail.com>
2024-02-14 13:42:46 +0800

    create app layout

---
+++ src/components/App/AppLayout.js
@@ -0,0 +1,26 @@
+export default function AppLayout() {
+    // State for managing search term and mobile navigation visibility
+    return ( 
+        <div>
+            <header>
+                <h1>This is the Header</h1>
+            </header>
+
+
+            <nav>
+                <h2>This is the Navbar</h2>
+            </nav>
+
+
+            <body>
+                <p>This is the content</p>
+            </body>
+
+
+            <footer>
+                <p>This is the footer</p>
+            </footer>
+            
+        </div>
+    )
+}
