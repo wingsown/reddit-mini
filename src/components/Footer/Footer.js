a0e49787fac01fe7b0ce850790a0ec4ac304359c
Wilson <wj.buena@gmail.com>
2024-03-03 08:48:22 +0800

    fix Footer structure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,12 @@ import footerCSS from './footer.module.css'
 function Footer() {
     return ( 
         <footer className={footerCSS.footer}>
-            <a>REDDIT</a>
-            <div>
-                <p><a>NavLink</a></p>
+            <a className={footerCSS.footer}>Reddit<span className={footerCSS.logoSpan}>Lite</span></a>
+            <div className={footerCSS.contact}>
+                <p><a><i>Link1</i></a></p>
+                <p><a><i>Link2</i></a></p>
+                <p><a><i>Link3</i></a></p>
+                <p><a><i>Link4</i></a></p>
             </div>
         </footer>
 
