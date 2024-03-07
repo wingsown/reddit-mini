b62185c7f3e9b68a9626661fafce582896451100
Wilson <wj.buena@gmail.com>
2024-03-07 13:37:37 +0800

    add styles to ArticlePreview

--- src/features/ArticlePreview/ArticlePreview.js
+++ src/features/ArticlePreview/ArticlePreview.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 import Media from "../Media/Media";
 import Title from "../Title/Title";
 import SubReddit from "../SubReddit/SubReddit";
+import articleCSS from "./articlePreview.module.css";
 
 const ArticlePreview = () => {
   return (
-    <div>
-      <Link>
+    <div className={articleCSS.card}>
+      <Link className={articleCSS.container}>
         <figure>
           <Media />
         </figure>
