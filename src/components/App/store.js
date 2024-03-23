7c533d9a953c3717805d9c17096f52cc790b8da0
Wilson <wj.buena@gmail.com>
2024-03-23 16:43:54 +0800

    Update Article component and add articleReducer to store

--- src/components/App/store.js
+++ src/components/App/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import articlePreviewReducer from "../../features/Category/categorySlice";
 import searchReducer from "../../features/Search/searchSlice";
+import articleReducer from "../../features/Article/articleSlice";
 
 export const store = configureStore({
   reducer: {
     articlePreviews: articlePreviewReducer,
     search: searchReducer,
+    article: articleReducer,
   },
 });
--- src/features/Article/Article.js
+++ src/features/Article/Article.js
@@ -1,11 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
 import Title from "../Title/Title";
+import SubReddit from "../SubReddit/SubReddit";
+import {
+  hasErrorArticle,
+  isLoadingArticle,
+  loadArticleById,
+  selectArticle,
+} from "./articleSlice";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Article = () => {
+  const dispatch = useDispatch();
+  const article = useSelector(selectArticle);
+  const isLoading = useSelector(isLoadingArticle);
+  const hasError = useSelector(hasErrorArticle);
+  const { id } = useParams();
+
+  useEffect(
+    () => {
+      dispatch(loadArticleById(id));
+    },
+    dispatch,
+    id
+  );
+
+  const { title, subReddit, score } = article;
   return (
     <section>
-      Article
       <div>
-        <Title />
+        <Title title={title} />
+        <SubReddit subReddit={subReddit} score={score} />
       </div>
     </section>
   );
