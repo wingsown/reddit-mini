e774d51d965ded45d66bc8afab02e87f8b77f828
Wilson <wj.buena@gmail.com>
2024-03-25 11:32:25 +0800

    completee Comments and Comment component

--- src/features/Comments/Comment.jsx
+++ src/features/Comments/Comment.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import commentsCSS from "./comments.module.css";
 
 const Comment = ({ comment }) => {
+  const [showReplies, setShowReplies] = useState();
+
+  const handleReplies = (e) => {
+    setShowReplies(!showReplies);
+  };
   return (
-    <div>
-      <p>{comment.author}</p>
-      <ReactMarkdown>{comment.text}</ReactMarkdown>
+    <div className={commentsCSS.comment}>
+      <p className={commentsCSS.author}>{comment.author}</p>
+      <ReactMarkdown className={commentsCSS.text}>{comment.text}</ReactMarkdown>
+      {comment.replies && (
+        <p className={commentsCSS.replyButton} onClick={handleReplies}>
+          {showReplies ? "Hide Replies" : "Show Replies"}
+        </p>
+      )}
+
+      {comment.replies?.map((reply) => {
+        return (
+          <div
+            className={showReplies ? commentsCSS.reply : commentsCSS.hideReply}
+          >
+            <div className={commentsCSS.comment}>
+              <p className={commentsCSS.author}>{reply.data.author}</p>
+              <ReactMarkdown className={commentsCSS.text}>
+                {reply.data.body}
+              </ReactMarkdown>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
